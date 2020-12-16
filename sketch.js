42bbd7d9844e8b09742c240623d90d71d6410930
AryaMahajan21 <72708375+AryaMahajan21@users.noreply.github.com>
2020-12-16 13:52:33 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -32,20 +32,7 @@ if (deformation<100){
   car.shapeColor = color(0,255,0);
 }
 }
-/*if (car.isTouching(wall)){
-  car.velocityX = 0;
-var deformation = 0.5 * weight * speed * speed/22509;
-if (deformation>180){
-  car.shapeColor = color(255,0,0);
 
-}
-if (deformation<180 && deformation > 100){
-  car.shapeColor = color(230,230,0);
-}
-if (deformation<100){
-  car.shapeColor = color(0,255,0);
-}
-}*/
   drawSprites();
   fill("blue");
   ellipse(car.x - 25,220,30);
